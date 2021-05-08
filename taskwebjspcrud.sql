37a07aab0d1a5e1df3fbcd0c7da20ebefc8fbbf0
sankar566 <60536375+sankar566@users.noreply.github.com>
2021-05-08 18:20:49 +0530

    Add files via upload

---
+++ taskwebjspcrud.sql
@@ -0,0 +1,67 @@
+create database taskwebjspcrud;
+SELECT * FROM taskwebjspcrud.student;
+
+TRUNCATE taskwebjspcrud.student;
+DESCRIBE taskwebjspcrud.student;
+SELECT *FROM taskwebjspcrud.student;
+
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (1, '20210001','raja','ECE','chennai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (2, '20210002','suresh','ECE','madurai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (3, '20210003','gowtham','ECE','tirunelveli');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (4, '20210004','Henry','ECE','chennai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (5, '20210005','Alexander','ECE','madurai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (6, '20210006','Advik','ECE','tirunelveli');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (7, '20210007','raja','ECE','chennai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (8, '20210008','suresh','ECE','madurai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (9, '20210009','gowtham','ECE','tirunelveli');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (10, '20210010','Oviyan','ECE','chennai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (11, '20210011','suresh','ECE','madurai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (12, '20210012','gowtham','ECE','tirunelveli');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (13, '20210013','Oviyan','ECE','tirunelveli');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (14, '20210014','raja','ECE','chennai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (15, '20210015','suresh','ECE','madurai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (16, '20210016','Alexander','ECE','tirunelveli');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (17, '20210017','raja','ECE','chennai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (18, '20210018','Oviyan','ECE','madurai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (19, '20210019','raja','ECE','chennai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (20, '20210020','Advik','ECE','madurai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (21, '20210021','suresh','ECE','madurai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (22, '20210022','Advik','ECE','tirunelveli');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (23, '20210023','gowtham','ECE','tirunelveli');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (24, '20210024','raja','ECE','chennai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (25, '20210025','Advik','ECE','madurai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (26, '20210066','Oviyan','ECE','tirunelveli');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (27, '20210027','raja','ECE','chennai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (28, '20210028','Oviyan','ECE','madurai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (29, '20210029','raja','ECE','chennai');
+INSERT INTO taskwebjspcrud.student (id,roll_number,student_name,dept,place) 
+VALUES (30, '20210030','Advik,'ECE','madurai');
